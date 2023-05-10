26976e2331373ce9c6b7a15c1910364340a4afbf
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-05-10 18:15:10 +0100

    Create Animal subclasses Bee and Bat

---
+++ AnimalPerspective/Bat.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalPerspective
+{
+    public class Bat : Animal
+    {
+       public override string Sound()
+        {
+            return "Squeak!";
+        } 
+    }
+}
